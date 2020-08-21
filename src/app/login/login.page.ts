3f9977de72b759a83760962d4798e00fb4b21cb3
Megantee1805 <44191183+Megantee1805@users.noreply.github.com>
2020-08-21 08:36:26 +0800

    made the menu hideable

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup
+  constructor() { 
+    this.loginForm = new FormGroup({ 
+      email: new FormControl(''), 
+      password: new FormControl('')
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
+  registerForm: FormGroup 
   constructor() { }
 
   ngOnInit() {
