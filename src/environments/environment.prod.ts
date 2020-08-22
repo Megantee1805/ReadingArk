3acf698364c99737ac0a086e3a29770e205358f2
Megantee1805 <44191183+Megantee1805@users.noreply.github.com>
2020-08-22 18:30:31 +0800

    start small

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true, 
-  firebase = {
+  firebase : {
     apiKey: "AIzaSyBccklnOaN3r8LYGm5uxA9rJsiglchtKjM",
     authDomain: "readingark.firebaseapp.com",
     databaseURL: "https://readingark.firebaseio.com",
@@ -9,5 +9,5 @@ export const environment = {
     messagingSenderId: "874644300729",
     appId: "1:874644300729:web:45394f3b3655082f790c68",
     measurementId: "G-DKYBVH1V8C"
-  };
+  }
 };
