affe76d9ff0ed40ebfe84df29d8dbd508779ade4
Megantee1805 <44191183+Megantee1805@users.noreply.github.com>
2020-08-22 18:35:51 +0800

    added the module

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private firebaseAuth : AngularFireAuth) { }
+
+  // signInWithEmail(email: string, password: string): Promise<auth.UserCredential> {
+  //   return this.firebaseAuth.signInWithEmailAndPassword(email, password);
+  // }
+
+  // signUpWithEmail(email: string, password: string): Promise<auth.UserCredential> {
+  //   return this.angularFire.createUserWithEmailAndPassword(email, password);
+  // }
 }
